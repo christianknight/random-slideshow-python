df97a2d42d42f1a718d6fe7ac8f314b030eade8e
Christian Knight <christianknight9@gmail.com>
2021-11-12 14:09:55 -0500

    Copy details from README to config.py setting descriptions

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # config.py
 
 # ----- Description of settings -----
-# duration - time, in seconds, between image transitions
-# img_directory - path to a folder containing image files (.jpg and .png only)
+# duration - time, in seconds, between image transitions. If no value is specified, the default duration is 4 seconds.
+# img_directory - path to a folder containing image files (.jpg and .png only). If no path is specified, the default image directory is the present working directory.
 # image_save_path - directory path for saving selected photos
 # topmost - boolean flag to set whether the slideshow is displayed on top of all other windows or not
-# size_max_x, size_max_y - maxiumum size, in pixels, to display the image at in the horizontal (x) or vertical (y) directions
-# position_x, position_y - maxiumum size, in pixels, to display the image at in the horizontal (x) or vertical (y) directions
+# size_max_x, size_max_y - maxiumum size, in pixels, to display the image at in the horizontal (x) or vertical (y) directions. If no values are specified, the default maximum values are determined by the current display resolution.
+# position_x, position_y - starting position, in pixels, from which the image is displayed relative to the top-left corner of the display, (0, 0). If no values are specified, the default starting position is (0, 0).
 # fullscreen - flag to indicate whether the slideshow should take up the full screen with black background
 # montage_mode - flag to indicate if montage mode is activated (several tiled images)
 # montage_size - number of photos to use in each montage when montage mode is activated
