54ce90a954add54c8e7b8cfb357d6bed5bb19f61
Christian Knight <christianknight9@gmail.com>
2021-11-12 14:05:57 -0500

    Tidy up config.py

--- config.py
+++ config.py
@@ -1,15 +1,33 @@
-# duration = 6			# time, in seconds, between image transitions
-# img_directory = "."		# path to a folder containing image files (.jpg and .png only)
-# image_save_path = '.'	# directory path for saving selected photos
-# topmost = False		# boolean flag to set whether the slideshow is displayed on top of all other windows or not
-# size_max_x = 96		# maxiumum size, in pixels, to display the image at in the horizontal (x) direction
-# size_max_y = 96		# maxiumum size, in pixels, to display the image at in the vertical (y) direction
-# position_x = 0		# starting horizontal position, in pixels, from which the image is displayed relative to the top-left corner of the display, (0, 0)
-# position_y = 0		# starting vertical position, in pixels, from which the image is displayed relative to the top-left corner of the display, (0, 0)
-# fullscreen = True		# flag to indicate whether the slideshow should take up the full screen with black background
-# montage_mode = True	# flag to indicate if montage mode is activated (several tiled images)
-# montage_size = 10		# number of photos to use in each montage when montage mode is activated
-# video_player_enable = True	# flag to indicate if the slideshow is in video mode
-# random = False        # flag to indicate whether to play the slideshow in random order or not
-# cursor_enable = True          # flag to indicate whether the mouse cursor should be shown on top of the slideshow or not
-# mouse_nudge = False           # flag to indicate whether to nudge the mouse cursor every time the slideshow advances (to keep the screensaver from activating)
+# config.py
+
+# ----- Description of settings -----
+# duration - time, in seconds, between image transitions
+# img_directory - path to a folder containing image files (.jpg and .png only)
+# image_save_path - directory path for saving selected photos
+# topmost - boolean flag to set whether the slideshow is displayed on top of all other windows or not
+# size_max_x, size_max_y - maxiumum size, in pixels, to display the image at in the horizontal (x) or vertical (y) directions
+# position_x, position_y - maxiumum size, in pixels, to display the image at in the horizontal (x) or vertical (y) directions
+# fullscreen - flag to indicate whether the slideshow should take up the full screen with black background
+# montage_mode - flag to indicate if montage mode is activated (several tiled images)
+# montage_size - number of photos to use in each montage when montage mode is activated
+# video_player_enable - flag to indicate if the slideshow is in video mode
+# random - flag to indicate whether to play the slideshow in random order or not
+# cursor_enable - flag to indicate whether the mouse cursor should be shown on top of the slideshow or not
+# mouse_nudge - flag to indicate whether to nudge the mouse cursor every time the slideshow advances (to keep the screensaver from activating)
+
+# ----- Settings -----
+# duration = 6
+# img_directory = '.'
+# image_save_path = '.'
+# topmost = False
+# size_max_x = 96
+# size_max_y = 96
+# position_x = 0
+# position_y = 0
+# fullscreen = True
+# montage_mode = True
+# montage_size = 10
+# video_player_enable = True
+# random = False
+# cursor_enable = True
+# mouse_nudge = False
